1428c1d69523223628817341bf078cfbd3a2be79
it24103936 <it24103936@my.sliit.lk>
2024-11-19 17:46:48 +0000

    add IT24103936Lab4Q3

---
+++ Q3/IT24103936Lab4Q3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IT24103936Lab4Q3 {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	int number = input.nextInt();
+
+	String result = (number>0) ? "Positive" : (number<0) ? "Negative" : "Zero";
+	System.out.println("The number is: " + result);
+	input.close();
+	}
+}
